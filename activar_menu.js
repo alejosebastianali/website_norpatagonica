31d135f1d5e34827343f78474a0d5a39683bfadd
alejosebastianali <74724530+alejosebastianali@users.noreply.github.com>
2022-11-23 12:46:00 -0300

    Add files via upload

---
+++ activar_menu.js
@@ -0,0 +1,107 @@
+//Esta accion maneja los sub submenu
+
+//La funcion outPopup busca el ID contenedor general y le aplica el estilo que remueve todo el popup cuando, se hace click en el boton de cerrar
+//function outPopup(){
+//    document.getElementById("popupPromociones").classList.toggle("submenu-remove");
+//}
+
+//Al hacer click en cerrar se activa la funcion que carga el estilo remove
+//document.getElementById("botonPopup").onclick = function(){
+//    outPopup();
+//}
+
+
+//Togle Mobile Menu
+$(function() {
+    $(".toggle").on("click", function() {
+        if ($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+            $(this).find("a").html("<i class='fas fa-bars'></i>");
+        } else {
+            $(".item").addClass("active");
+            $(this).find("a").html("<i class='fas fa-times'></i>");
+        }
+    });
+});
+
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+
+function toggleMenu() {
+    if(menu.classList.contains("active")){
+        menu.classList.remove("active");
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else{
+        menu.classList.add("active");
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+}
+
+toggle.addEventListener('click', toggleMenu, false)
+
+
+
+//Submenu
+const items = document.querySelectorAll('.item');
+
+function toggleItem() {
+    if (this.classList.contains("submenu-active")) {
+        this.classList.remove("submenu-active");
+    } else if (menu.querySelector(".submenu-active")){
+        menu.querySelector(".submenu-active").classList.remove("submenu-active");
+        this.classList.add("submenu-active");
+    } else {
+        this.classList.add("submenu-active");
+    }
+}
+
+for (let item of items) {
+    if (item.querySelector('.submenu')) {
+        item.addEventListener('click', toggleItem, false);
+        item.addEventListener('keypress', toggleItem, false);
+    }
+}
+
+document.addEventListener('click', closeSubmenu, false);
+
+
+
+//Submenu Submenu
+
+const items2 = document.querySelectorAll('.item2');
+
+function toggleItem2() {
+    if (this.classList.contains("submenu-active2")) {
+        this.classList.remove("submenu-active2");
+    } else if (menu.querySelector(".submenu-active2")){
+        menu.querySelector(".submenu-active2").classList.remove("submenu-active2");
+        this.classList.add("submenu-active2");
+    } else {
+        this.classList.add("submenu-active2");
+    }
+}
+
+for (let item of items2) {
+    if (item.querySelector('.submenu2')) {
+        item.addEventListener('click', toggleItem2, false);
+        item.addEventListener('keypress', toggleItem2, false);
+    }
+}
+
+document.addEventListener('click', closeSubmenu2, false);
+
+//function closeSubmenu(e) {
+//    let isClickInside = menu.contains(e.target);
+//
+//    if (!isClickInside && menu.querySelector('.submenu-active')){
+//        menu.querySelector('submenu-active').classList.remove('.submenu-active');
+//    }
+//}
+
+
+
+
+
+
+
+
